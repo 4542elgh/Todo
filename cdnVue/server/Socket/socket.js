9a4108a6be03fe5731a6c7d7a94b3a5e1e71c106
winterMute84 <wcarbajal84@gmail.com>
2018-05-05 22:19:41 -0700

    added new db functions to websockets

--- cdnVue/server/Socket/socket.js
+++ cdnVue/server/Socket/socket.js
@@ -14,9 +14,23 @@ module.exports = (server, db) => {
                 .catch(err => io.emit('project-already-exists', err)) //if error, error message is emitted back
         })
 
+        //NO io.emit needed front end should call refresh-project
+        socket.on('delete-project',(projectName)=>{
+            db.deleteProject(projectName)
+            .then(result => {return})
+            .catch(err => io.emit('error-deleting-project', err))
+        })
+
+        socket.on('edit-project-name', (oldName, newName)=>{
+            db.editProjectName(oldName,newName)
+            .then(updatedProject => io.emit('updated-project-name', updatedProject.name))
+            .catch(err => io.emit('error-editing-project-name', err))
+        })
+
+        
         socket.on('add-todo', (projectName, todo) => {
             db.addTodo(projectName, todo)
-                .then(todo => io.emit('added-todo', todo))
+                .then(project => io.emit('added-todo', project.todos))
                 .catch(err => io.emit('todo-already-exists', err))
         })
 
@@ -28,6 +42,20 @@ module.exports = (server, db) => {
                 .then(project => io.emit('todo-toggled-inProject',project.todos))
             })
             .catch(err => io.emit('todo-toToggle-doesNotExists', err))
+        })
+        
+        socket.on('delete-todo',(projectName, desc) =>{
+
+            db.deleteTodo(projectName, desc)
+            .then(project => io.emit('existing-todos', project.todos))
+            .cathc(err => io.emit('error-on-deletion-ofTodo', err))
+
+        })
+
+        socke.on('edit-todo',(projectName, oldDesc, newDesc) =>{
+            db.editTodo(projectName,oldDesc,newDesc)
+            .then(project => io.emit('updated-todo',project,todos))
+            .catch(err => io.emit('error-updating-todo', err))
         })
 
         socket.on('remove-completed-todos',( projectName) =>{
